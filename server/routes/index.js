3c50b2f8d4e5f2fef077601d82cab016e1085a9d
Alice S <alices@Alices-MacBook-Pro.local>
2022-10-01 15:18:27 -0700

    testing reading from DB

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
+const mongoose = require('mongoose');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/users', async function (req, res, next) {
+  try {
+    const user = await User.findById("6338b7d0f3a1b6bb62acf747");
+    res.json(user);
+    console.log("user: ", user);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
+
+
