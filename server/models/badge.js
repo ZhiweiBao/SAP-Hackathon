16cf14329fdc1efefb80e8b58b9b4f6ddf403a7f
Liam Bao <baozhiw@amazon.com>
2022-10-01 14:31:16 -0700

    Clean up, add mongoose package

--- server/models/badge.js
+++ server/models/badge.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
 
 const BadgeSchema = new Schema({
     title: String,
     image: String,
     challengeId: { type: Schema.Types.ObjectId, ref: "Challenge" },
 });
-
-UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('Badge', BadgeSchema);
--- server/models/event.js
+++ server/models/event.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const User = require('./user');
-const passportLocalMongoose = require('passport-local-mongoose');
 
 const EventSchema = new Schema({
     title: String,
@@ -13,7 +11,5 @@ const EventSchema = new Schema({
     attendees: [{ type: Schema.Types.ObjectId, ref: "User" }],
     creator: { type: Schema.Types.ObjectId, ref: "User" },
 });
-
-UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('Event', EventSchema);
