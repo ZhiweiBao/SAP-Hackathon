ccad758148416c2e91e37bf2b92ebcf3cbfb75f9
Liam Bao <baozhiw@amazon.com>
2022-10-01 18:56:17 -0700

    Add API calls in client

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -1,91 +1,93 @@
 const serverUrl = process.env.REACT_APP_SERVER_URL
 
-//fetch user data
+//fetch all users
+export async function fetchAllUsers() {
+  try {
+    const response = await fetch(`${serverUrl}/users`);
+    return await response.json();
+  } catch (err) {
+    console.log(`Fetch failed: user list.\n Error: ${err}`);
+  }
+}
+
+// fetch user data
 export async function fetchUserByID(id) {
   try {
-    const response = await fetch(
-      `${serverUrl}/users/${id}`);
-    if (!response.ok) {
-      throw Error("Fetch failed: user infomation");
-    }
-    const data = await response.json();
-    return data;
+    const response = await fetch(`${serverUrl}/users/${id}`);
+    return await response.json();
   } catch (err) {
-    console.log("err", err);
+    console.log(`Fetch failed: user information.\n Error: ${err}`);
   }
 }
 
-//fetch event data
-export async function fetchEventByID(id) {
+// fetch user data by email
+export async function fetchUserByEmail(email) {
   try {
-    const response = await fetch(
-      `${serverUrl}/events/${id}`);
-    if (!response.ok) {
-      throw Error("Fetch failed: event");
-    }
-    const data = await response.json();
-    return data;
+    const response = await fetch(`${serverUrl}/users/email/${email}`);
+    return await response.json();
   } catch (err) {
-    console.log("err", err);
+    console.log(`Fetch failed: user information.\n Error: ${err}`);
   }
 }
 
+// fetch all events
 export async function fetchAllEvents() {
   try {
     const response = await fetch(`${serverUrl}/events`);
-
-    if (!response.ok) {
-      throw Error("Fetch failed: all events");
-    }
-    const data = await response.json();
-    return data;
+    return await response.json();
   } catch (err) {
-    console.log("err", err);
+    console.log(`Fetch failed: event list.\n Error: ${err}`);
   }
 }
 
+// fetch event data
+export async function fetchEventByID(id) {
+  try {
+    const response = await fetch(`${serverUrl}/events/${id}`);
+    return await response.json();
+  } catch (err) {
+    console.log(`Fetch failed: event information.\n Error: ${err}`);
+  }
+}
 
-// export async function fetchTrailsByMode(mode) {
-//   try {
-//     const response = await fetch(`/api/trails/search/mode/${mode}`);
+// fetch all badges
+export async function fetchAllBadges() {
+  try {
+    const response = await fetch(`${serverUrl}/badges`);
+    return await response.json();
+  } catch (err) {
+    console.log(`Fetch failed: badge list.\n Error: ${err}`);
+  }
+}
 
-//     if (!response.ok) {
-//       throw Error("Fetch failed");
-//     }
-//     const data = await response.json();
-//     return data;
-//   } catch (err) {
-//     console.log("err", err);
-//   }
-// }
+// fetch badge data
+export async function fetchBadgeByID(id) {
+  try {
+    const response = await fetch(`${serverUrl}/badges/${id}`);
+    return await response.json();
+  } catch (err) {
+    console.log(`Fetch failed: badge information.\n Error: ${err}`);
+  }
+}
 
-// // export async function fetchReviewsbyTrail(id) {
-// //   try {
-// //     const response = await fetch(`/api/reviews/review?trail_id=${id}`);
+// fetch all challenges
+export async function fetchAllChallenges() {
+  try {
+    const response = await fetch(`${serverUrl}/challenges`);
+    return await response.json();
+  } catch (err) {
+    console.log(`Fetch failed: challenge list.\n Error: ${err}`);
+  }
+}
 
-// //     if (!response.ok) {
-// //       throw Error("Fetch failed");
-// //     }
-// //     const data = await response.json();
-// //     return data;
-// //   } catch (err) {
-// //     console.log("err", err);
-// //   }
-// // }
-
-// // export async function fetchReviewsbyUser(id) {
-// //   try {
-// //     const response = await fetch(`/api/reviews/review?user_id=${id}`)
-// //     if (!response.ok) {
-// //       throw Error("Fetch failed");
-// //     }
-// //     const data = await response.json();
-// //     return data;
-// //   } catch (err) {
-// //     console.log("err", err);
-// //   }
-// // }
-
-
+// fetch challenge data
+export async function fetchChallengeByID(id) {
+  try {
+    const response = await fetch(`${serverUrl}/challenges/${id}`);
+    return await response.json();
+  } catch (err) {
+    console.log(`Fetch failed: challenge information.\n Error: ${err}`);
+  }
+}
 
 
